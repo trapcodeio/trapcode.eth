ba588b0acdd292c29a9c48cc234e3b0b48e32e9b
trapcodeio <trapcode.io@gmail.com>
2022-07-13 13:17:06 +0100

    Set title from vite-ejs-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    ViteEjsPlugin({
+      title: "TrapCode.eth"
+    })
+  ]
 });
